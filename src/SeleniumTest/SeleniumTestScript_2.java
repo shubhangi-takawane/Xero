a1e1581e2387fba9f72b7707ee5c26696290085e
Shubhangi Takawane <Shubhangi.Takawane@walmartlabs.com>
2018-10-01 22:55:05 -0700

    remove print statement

--- src/SeleniumTest/SeleniumTestScript_2.java
+++ src/SeleniumTest/SeleniumTestScript_2.java
@@ -140,7 +140,7 @@ public class SeleniumTestScript_2 {
 				actData.add(ele.getText().trim());
 			}
 			
-			System.out.println("ActData"+actData);
+			System.out.println(actData);
 			
 			if(actData.equals(expList))
 				System.out.println("Error message is displayed");
